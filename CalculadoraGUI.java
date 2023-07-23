fd407bb66ece8b49208ef3c96cf25935597ba83f
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2023-07-22 22:52:16 -0500

    Se añadió la nueva interfaz con fondo negro y disposición de botones

--- CalculadoraGUI.java
+++ CalculadoraGUI.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -5,7 +6,10 @@ import java.awt.event.ActionListener;
 public class CalculadoraGUI extends JFrame  implements ActionListener  {
     private JTextField pantalla;
     private JButton [] botonesNumeros;
-    private JButton bsuma, bresta, bigual, blimpiar, bdivision, bmultiplicacion, bborrar;
+    private JButton bsuma, bresta, bigual, 
+                blimpiar, bdivision, bmultiplicacion, 
+                bborrar, bblanco;
+    private ArrayList<JButton> AllButtons = new ArrayList<>();
     private double NumeroTemporal;
     private char operacion;
 
@@ -25,44 +29,66 @@ public class CalculadoraGUI extends JFrame  implements ActionListener  {
         botonesNumeros = new JButton[10];
         for (int i = 0; i < 10; i++) {
             botonesNumeros[i] = new JButton(String.valueOf(i));
+            AllButtons.add(botonesNumeros[i]);
             botonesNumeros[i].addActionListener(this);
         }
 
         // Crear botones para las operaciones
         bsuma = new JButton("+");
+        AllButtons.add(bsuma);
         bsuma.addActionListener(this);
         
         bresta = new JButton("-");
+        AllButtons.add(bresta);
         bresta.addActionListener(this);
 
         bigual = new JButton("=");
+        AllButtons.add(bigual);
         bigual.addActionListener(this);
 
         blimpiar = new JButton("C");
+        AllButtons.add(blimpiar);
         blimpiar.addActionListener(this);
 
         bdivision = new JButton("÷");
+        AllButtons.add(bdivision);
         bdivision.addActionListener(this);
 
         bmultiplicacion = new JButton("x");
+        AllButtons.add(bmultiplicacion);
         bmultiplicacion.addActionListener(this);
 
-        bborrar = new JButton("⌂ DEL")
+        bborrar = new JButton("⌂ DEL");
+        AllButtons.add(bborrar);
         bborrar.addActionListener(this);
 
+        bblanco = new JButton("");
+        AllButtons.add(bblanco);
 
+        // Interfaz de todos los botones
+
+        for(JButton s : AllButtons) {
+            s.setBackground(new Color(0, 0, 0));
+            s.setForeground(new Color(250, 250, 250));
+            Font newfont = new Font("Century Gothic", Font.BOLD, 18);
+            s.setFont(newfont);
+
+        }
 
 
         // panel de Operaciones y cero
         JPanel paneloperaciones = new JPanel();
-        paneloperaciones.setLayout(new GridLayout(5, 3, 1, 1));
+        paneloperaciones.setLayout(new GridLayout(6, 3, 1, 1));
         for (int i = 1; i <= 9; i++) paneloperaciones.add(botonesNumeros[i]);
         paneloperaciones.add(botonesNumeros[0]);
+        paneloperaciones.add(bborrar);
+        paneloperaciones.add(blimpiar);
         paneloperaciones.add(bsuma);
         paneloperaciones.add(bresta);
         paneloperaciones.add(bmultiplicacion);
         paneloperaciones.add(bdivision);
         paneloperaciones.add(bigual);
+        paneloperaciones.add(bblanco);
 
         
         
